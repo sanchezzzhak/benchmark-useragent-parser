6602bdaa1203c33f5146850f05b3ec8bfbcd01f7
Tutik Alexsandr <sanchezzzhak@yandex.ru>
2021-03-03 05:32:05 +0300

    feat(parser) prototype run all parsers

--- src/Command/Analyze.php
+++ src/Command/Analyze.php
@@ -9,32 +9,61 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+
 /**
  * Class Analyze
  * @package App\Command
  */
 class Analyze extends Command
 {
-    /*** @var string */
-    protected static $defaultName = 'analyze:start';
+
+    protected static string $defaultName = 'analyze:start';
 
     protected function configure()
     {
         $this->addArgument(
-            'use',
-            InputArgument::REQUIRED | InputArgument::IS_ARRAY,
-            sprintf('Select parses (%s)', implode(', ', ['*' /*...self::PARSERS*/])),
-        );
-        $this->addArgument(
-            'folder',
-            InputArgument::REQUIRED,
-            'Set folder save reports'
+            'report',
+            InputArgument::OPTIONAL,
+            'Set report name'
         );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $uses = $input->getArgument('use');
+        $basePath = realpath(__DIR__ . '/../Parser');
+        $reportFolder = date('Y-m-d');
+        $scriptFolders = glob($basePath . '/*', GLOB_ONLYDIR);
+        foreach ($scriptFolders as $folder) {
+            $folderName = pathinfo($folder, PATHINFO_BASENAME);
+            $output->writeln("ready to start parser {$folderName}");
+//            continue;
 
+            $phpScriptPath = realpath($folder . DIRECTORY_SEPARATOR . "parser.php");
+            if (is_file($phpScriptPath)) {
+
+                $command = implode(' ', [
+                    'php',
+                    $phpScriptPath,
+                    '--fixtures="data/paths.json"',
+                    sprintf('--report="data/%s/fixture-%s.log"', $reportFolder, $folderName)
+                ]);
+                $process = new Process($command);
+                var_dump($process->getCommandLine());
+
+                $process->start();
+                do {
+                    $process->checkTimeout();
+                } while ($process->isRunning() && (sleep(1) !== false));
+                if (!$process->isSuccessful()) {
+                    throw new \Exception($process->getErrorOutput());
+                }
+            }
+            if (is_file($folder . DIRECTORY_SEPARATOR . "parser.js")) {
+
+            }
+        }
     }
+
 }
