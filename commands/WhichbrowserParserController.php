4527f45dbcb1f43217f5e1b9ae6d7d721d9512f8
sanchezzzhak <sanchezzzhak@ya.ru>
2021-09-09 17:56:37 +0300

    feat(common) port symphony parser whichbrowser to yii2 controller

---
+++ commands/WhichbrowserParserController.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace app\commands;
+
+use app\helpers\ParserConfig;
+use app\models\BenchmarkResult;
+use app\models\DeviceDetectorResult;
+use yii\console\Controller;
+use yii\console\ExitCode;
+use WhichBrowser\Parser;
+
+class WhichbrowserParserController extends Controller
+{
+
+    public function actionIndex()
+    {
+        $parserId = ParserConfig::getSourceIdByRepository(
+            ParserConfig::PROJECT_WHICHBROWSER_PARSER
+        );
+        $query = BenchmarkResult::find();
+
+        /** @var BenchmarkResult $row */
+        foreach ($query->each() as $row) {
+            $useragent = $row->user_agent;
+            $this->stdout(sprintf('#%s parse %s', $row->id, $useragent) . PHP_EOL);
+
+            $parser = null;
+            $useragentHeader = 'UserAgent: ' . $useragent;
+            $info = Benchmark::benchmarkWithCallback(function () use (&$parser, $useragentHeader) {
+                $parser = new Parser($useragentHeader, ['detectBots' => true]);
+            });
+
+            $model = DeviceDetectorResult::findOrCreate($row->id, $parserId);
+            $model->time = $info['time'];
+            $model->memory = $info['memory'];
+
+            $detectResult = [
+                'browser' => $parser->browser->toArray(),
+                'engine' => $parser->engine->toArray(),
+                'os' => $parser->os->toArray(),
+                'device' => $parser->device->toArray()
+            ];
+
+            $deviceType = $detectResult['device']['type'] ?? '';
+            $model->device_type = $deviceType;
+            $model->data_json = json_encode($detectResult);
+
+            if ($deviceType === 'bot') {
+                $model->is_bot = true;
+                $model->bot_name  = $detectResult['browser']['name'] ?? '';
+                $model->save();
+                continue;
+            }
+
+            $model->client_name = $detectResult['browser']['name'] ?? '';
+            $model->client_type = $detectResult['browser']['type'] ?? '';
+            if (isset($detectResult['browser']['version']) && is_array($detectResult['browser']['version'])) {
+                $model->client_version = $detectResult['browser']['version']['value'] ?? '';
+            } else {
+                $model->client_version = $detectResult['browser']['version'] ?? '';
+            }
+
+            $model->engine_name = $detectResult['engine']['name'] ?? '';
+            $model->engine_version = $detectResult['engine']['version'] ?? '';
+
+            $model->brand_name = $detectResult['device']['manufacturer'] ?? '';
+            $model->model_name = $detectResult['device']['model'] ?? '';
+            $model->save();
+        }
+
+        return ExitCode::OK;
+    }
+
+
+
+
+}
--- readme.md
+++ readme.md
@@ -27,6 +27,7 @@ Commands
 | `php yii robbing false` | robbing new useragents to database (params:  true - check exist ua in db)  |
 | `php yii matomo-parser`  | analyze all useragents and save result to db |
 | `php yii mimmi20-parser`  | analyze all useragents and save result to db |
+| `php yii whichbrowser-parser`  | analyze all useragents and save result to db |
 
 
 Results For 2021-03-04
