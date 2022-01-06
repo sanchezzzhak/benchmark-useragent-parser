7c3b930a4e40f5136731987eb8ad995407824f08
Tutik Alexsandr <sanchezzzhak@ya.ru>
2022-01-06 15:10:43 +0300

    feat(config): create dist mysql config

--- config/db.php
+++ config/db.php
@@ -1,14 +1,16 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'sqlite:' . dirname(__DIR__, 1)  .'/runtime/data.db',
+return is_file(__DIR__ . '/local/db.php')
+    ? require __DIR__ . '/local/db.php'
+    : [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'sqlite:' . dirname(__DIR__, 1) . '/runtime/data.db',
 //    'username' => 'root',
 //    'password' => '',
-    'charset' => 'utf8',
+        'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+        // Schema cache options (for production environment)
+        //'enableSchemaCache' => true,
+        //'schemaCacheDuration' => 60,
+        //'schemaCache' => 'cache',
+    ];
---
+++ config/dist/mysql-db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=benchmark-useragent-parser',
+    'username' => 'root',
+    'password' => 'mysql',
+    'charset' => 'utf8',
+];
